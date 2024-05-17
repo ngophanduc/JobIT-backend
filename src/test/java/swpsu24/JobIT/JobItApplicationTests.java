3ad9c21d01085868a31374f79c8050bfe1670379
ngophanduc <phanduc01213@gmail.com>
2024-05-17 20:37:31 +0700

    abc

---
+++ src/test/java/swpsu24/JobIT/JobItApplicationTests.java
@@ -0,0 +1,13 @@
+package swpsu24.JobIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
